956700e1a31f39583c4ad626b7869eb9a4f96301
Raian Ruku <raianruku21@gmail.com>
2022-09-17 02:15:35 +0600

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import { Route } from 'react-router-dom';
+import aboutus from './aboutus';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Route path="/aboutus">About Us</Route>
     </div>
+    
   );
 }
 
