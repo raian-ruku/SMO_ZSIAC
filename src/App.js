cb996f06140955b60184689bdfeb91234652b0e1
Raian Ruku <raianruku21@gmail.com>
2022-09-17 18:07:04 +0000

    Sep 18, 2022, 12:07 AM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Router, Route } from 'react-router-dom';
 import aboutus from './aboutus';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
 <nav>
-  
+  <Link to="/aboutus">About us</Link>
 </nav>
 
   );
