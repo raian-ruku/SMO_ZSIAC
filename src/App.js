f8195930632d4b85cf3bd27c34fd65bdfecbd8c5
Raian Ruku <raianruku21@gmail.com>
2022-09-18 18:01:07 +0000

    Sep 19, 2022, 12:01 AM

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route } from 'react-router-dom';
-import aboutus from "./aboutus";
-import './App.css';
+import about from "./aboutus";
 import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
@@ -13,7 +12,7 @@ function App(){
 <h1>Home Page</h1>
 <BrowserRouter>
 <Route path="/aboutus">
-  <aboutus />
+  <about />
 </Route>
 </BrowserRouter>
 </div>
