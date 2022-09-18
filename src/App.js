51b70335cde7b41c72daef78e9fa66944245095b
Raian Ruku <raianruku21@gmail.com>
2022-09-18 17:17:42 +0000

    Sep 18, 2022, 11:17 PM

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App(){
 <h1>Home Page</h1>
 
 
+
   );
 };
 
--- src/aboutus.js
+++ src/aboutus.js
@@ -1,5 +1,5 @@
-const about =() => {
+function aboutus(){
     return <h1>About Us</h1>
 }
 
-export default about;
+export default aboutus;
